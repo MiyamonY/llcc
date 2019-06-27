9195525474c727f3992a9017746f9a81adc4edf1
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-06-27 11:13:16 +0900

    Add support add and sum

--- llcc.rkt
+++ llcc.rkt
@@ -2,16 +2,59 @@
 
 (require racket/cmdline)
 
-(define return-value
+(define expr
   (command-line
-   #:args (value)
-   value))
+   #:args (expr)
+   expr))
 
-(displayln ".intel_syntax noprefix")
-(displayln ".global main")
-(displayln "main:")
-(displayln (format "\tmov rax, ~a" return-value))
-(displayln "\tret")
+(define (take-while lst f)
+  (cond
+    ((null? lst) (values '() '()))
+    ((f (car lst))
+     (let-values ([(take rest)
+                   (take-while (cdr lst) f)])
+       (values (cons (car lst) take) rest)))
+    (else
+     (values '() lst))))
 
 (module+ test
-  (require rackunit))
+  (require rackunit)
+  (let-values ([(taken rest)
+                (take-while (string->list "12 + 34") char-numeric?)])
+    (check-equal? taken '(#\1 #\2)))
+
+  (let-values ([(taken rest)
+                (take-while (string->list "") char-numeric?)])
+    (check-equal? taken '())))
+
+
+(define (parse lst)
+  (cond ((null? lst) '())
+        ((eq? (car lst) #\+)
+         (define-values (taken rest) (take-while (cdr lst) char-numeric?))
+         (cons (format "\tadd rax, ~a" (list->string taken)) (parse rest)))
+        ((eq? (car lst) #\-)
+         (define-values (taken rest) (take-while (cdr lst) char-numeric?))
+         (cons (format "\tsub rax, ~a" (list->string taken)) (parse rest)))
+        ((char-numeric? (car lst))
+         (define-values (taken rest) (take-while lst char-numeric?))
+         (cons (format "\tmov rax, ~a" (list->string taken)) (parse rest)))
+        (else (error "failed: invalid value" (car lst)))))
+
+(module+ test
+  (check-equal?
+   (parse (string->list "12")) '("\tmov rax, 12"))
+
+  (check-equal?
+   (parse (string->list "12+34")) '("\tmov rax, 12" "\tadd rax, 34"))
+  (check-equal?
+   (parse (string->list "12+34-12")) '("\tmov rax, 12" "\tadd rax, 34" "\tsub rax, 12")))
+
+(define (main)
+  (displayln ".intel_syntax noprefix")
+  (displayln ".global main")
+  (displayln "main:")
+  (displayln (string-join (parse (string->list expr)) "\n"))
+  (displayln "\tret"))
+
+(main)
