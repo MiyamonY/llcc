c65e639b88d9ef2aa0856501aca0adb16ef2aa97
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2019-07-02 01:15:57 +0900

    Refactor
    * Redefine node

--- llcc.rkt
+++ llcc.rkt
@@ -19,6 +19,12 @@
 (define (token-return char-at)
   (token 'return "return" char-at))
 
+(define (token-lparen char-at)
+  (token 'paren "(" char-at))
+
+(define (token-rparen char-at)
+  (token 'paren ")" char-at))
+
 (define (token-if char-at)
   (token 'if "if" char-at))
 
@@ -29,16 +35,10 @@
   (token 'operator op char-at))
 
 (define (token-plus char-at)
-  (token-operator #\+ char-at))
+  (token-operator "+" char-at))
 
 (define (token-sub char-at)
-  (token-operator #\- char-at))
-
-(define (token-lparen char-at)
-  (token-operator #\( char-at))
-
-(define (token-rparen char-at)
-  (token-operator #\) char-at))
+  (token-operator "-" char-at))
 
 (define (token-eq char-at)
   (token-operator "==" char-at))
@@ -47,19 +47,19 @@
   (token-operator "!=" char-at))
 
 (define (token-lt char-at)
-  (token-operator #\< char-at))
+  (token-operator "<" char-at))
 
 (define (token-le char-at)
   (token-operator "<=" char-at))
 
 (define (token-gt char-at)
-  (token-operator #\> char-at))
+  (token-operator ">" char-at))
 
 (define (token-ge char-at)
   (token-operator ">=" char-at))
 
 (define (token-assign char-at)
-  (token-operator #\= char-at))
+  (token-operator "=" char-at))
 
 (define (token-number? token)
   (equal? (token-type token) 'number))
@@ -83,28 +83,28 @@
   (equal? (token-type token) 'operator))
 
 (define (token-plus? token)
-  (and (token-operator? token) (equal? (token-val token) #\+)))
+  (and (token-operator? token) (equal? (token-val token) "+")))
 
 (define (token-add? token)
   (token-plus? token))
 
 (define (token-minus? token)
-  (and (token-operator? token) (equal? (token-val token) #\-)))
+  (and (token-operator? token) (equal? (token-val token) "-")))
 
 (define (token-sub? token)
   (token-minus? token))
 
 (define (token-mul? token)
-  (and (token-operator? token) (equal? (token-val token) #\*)))
+  (and (token-operator? token) (equal? (token-val token) "*")))
 
 (define (token-div? token)
-  (and (token-operator? token) (equal? (token-val token) #\/)))
+  (and (token-operator? token) (equal? (token-val token) "/")))
 
 (define (token-lparen? token)
-  (and (token-operator? token) (equal? (token-val token) #\()))
+  (and (equal? (token-type token) 'paren) (equal? (token-val token) "(")))
 
 (define (token-rparen? token)
-  (and (token-operator? token) (equal? (token-val token) #\))))
+  (and (equal? (token-type token) 'paren) (equal? (token-val token) ")")))
 
 (define (token-eq? token)
   (and (token-operator? token) (equal? (token-val token) "==")))
@@ -113,19 +113,19 @@
   (and (token-operator? token) (equal? (token-val token) "!=")))
 
 (define (token-lt? token)
-  (and (token-operator? token) (equal? (token-val token) #\<)))
+  (and (token-operator? token) (equal? (token-val token) "<")))
 
 (define (token-le? token)
   (and (token-operator? token) (equal? (token-val token) "<=")))
 
 (define (token-gt? token)
-  (and (token-operator? token) (equal? (token-val token) #\>)))
+  (and (token-operator? token) (equal? (token-val token) ">")))
 
 (define (token-ge? token)
   (and (token-operator? token) (equal? (token-val token) ">=")))
 
 (define (token-assign? token)
-  (and (token-operator? token) (equal? (token-val token) #\=)))
+  (and (token-operator? token) (equal? (token-val token) "=")))
 
 (define (take-while lst f)
   (cond
@@ -162,7 +162,7 @@
           [(equal? (peek lst) #\space)
            (tokenize-rec (rest lst) (add1 char-at))]
           [(member (peek lst) '(#\+ #\- #\* #\/))
-           (cons (token-operator (peek lst) char-at)
+           (cons (token-operator (string (peek lst)) char-at)
                  (tokenize-rec (rest lst) (add1 char-at)))]
           [(equal? (peek lst) #\()
            (cons (token-lparen char-at) (tokenize-rec (rest lst) (add1 char-at)))]
@@ -235,93 +235,72 @@
   (raise-user-error
    'parse-error  "\n~a\n~a^\n~a\n" expr (make-string char-at #\space) msg))
 
-(struct node (type left right val [offset #:auto #:mutable])
-  #:auto-value 0
+(struct node-super ()
   #:transparent)
 
-(define (node-number num)
-  (node 'number null null num))
+(struct node-number node-super (val)
+  #:transparent)
 
-(define (node-assign left right)
-  (node 'assign left right #\=))
+(struct node-assign node-super (left right)
+  #:transparent)
 
-(define (node-return left)
-  (node 'return left null "return"))
+(struct node-local-variable node-super (name offset)
+  #:transparent)
 
-(define (node-local-variable name offset)
-  (define local-variable (node 'local-variable null null name))
-  (set-node-offset! local-variable offset)
-  local-variable)
+(struct node-return node-super (expr)
+  #:transparent)
 
-(define (node-if expr-pred stmt-true stmt-false)
-  (node 'if expr-pred stmt-true stmt-false))
+(struct node-if node-super (conditional true-clause false-clause)
+  #:transparent)
 
-(define (node-operator left right op)
-  (node 'operator left right op))
+(struct node-operator (op left right)
+  #:transparent)
 
 (define (node-plus left right)
-  (node-operator left right #\+))
+  (node-operator "+" left right))
 
 (define (node-sub left right)
-  (node-operator left right #\-))
+  (node-operator "-" left right ))
 
 (define (node-eq left right)
-  (node-operator left right "=="))
+  (node-operator "==" left right))
 
 (define (node-neq left right)
-  (node-operator left right "!="))
+  (node-operator "!=" left right))
 
 (define (node-lt left right)
-  (node-operator left right #\<))
+  (node-operator "<" left right))
 
 (define (node-le left right)
-  (node-operator left right "<="))
-
-(define (node-number? node)
-  (equal? (node-type node) 'number))
-
-(define (node-local-variable? node)
-  (equal? (node-type node) 'local-variable))
-
-(define (node-assign? node)
-  (equal? (node-type node) 'assign))
-
-(define (node-return? node)
-  (equal? (node-type node) 'return))
-
-(define (node-if? node)
-  (equal? (node-type node) 'if))
-
-(define (node-operator? node)
-  (equal? (node-type node) 'operator))
+  (node-operator "<=" left right))
 
 (define (node-add? node)
-  (and (node-operator? node) (equal? (node-val node) #\+)))
+  (and (node-operator? node) (equal? (node-operator-op node) "+")))
 
 (define (node-sub? node)
-  (and (node-operator? node) (equal? (node-val node) #\-)))
+  (and (node-operator? node) (equal? (node-operator-op node) "-")))
 
 (define (node-mul? node)
-  (and (node-operator? node) (equal? (node-val node) #\*)))
+  (and (node-operator? node) (equal? (node-operator-op node) "*")))
 
 (define (node-div? node)
-  (and (node-operator? node) (equal? (node-val node) #\/)))
+  (and (node-operator? node) (equal? (node-operator-op node) "/")))
 
 (define (node-eq? node)
-  (and (node-operator? node) (equal? (node-val node) "==")))
+  (and (node-operator? node) (equal? (node-operator-op node) "==")))
 
 (define (node-neq? node)
-  (and (node-operator? node) (equal? (node-val node) "!=")))
+  (and (node-operator? node) (equal? (node-operator-op node) "!=")))
 
 (define (node-lt? node)
-  (and (node-operator? node) (equal? (node-val node) #\<)))
+  (and (node-operator? node) (equal? (node-operator-op node) "<")))
 
 (define (node-le? node)
-  (and (node-operator? node) (equal? (node-val node) "<=")))
+  (and (node-operator? node) (equal? (node-operator-op node) "<=")))
 
 (define (reverse-compare op)
-  (cond [(equal? op #\>) #\<]
-        [(equal? op #\<) #\>]
+  (cond [(equal? op ">") "<"]
+        [(equal? op "<") ">"]
         [(equal? op "<=") ">="]
         [(equal? op ">=") "<="]))
 
@@ -385,7 +364,7 @@
       (cond [(null? tokens) (values unary0 tokens)]
             [(or (token-mul? (car tokens)) (token-div? (car tokens)))
              (let-values ([(unary1 remaining) (unary (cdr tokens))])
-               (mul-rec (node-operator unary0 unary1 (token-val (car tokens))) remaining))]
+               (mul-rec (node-operator (token-val (car tokens)) unary0 unary1) remaining))]
             [else (values unary0 tokens)]))
     (call-with-values (lambda () (unary tokens)) mul-rec))
 
@@ -396,7 +375,7 @@
             [(or (token-add? (first tokens)) (token-sub? (first tokens)))
              (let-values ([(operator) (first tokens)]
                           [(mul1 remaining) (mul (rest tokens))])
-               (add-rec (node-operator mul0 mul1 (token-val operator)) remaining))]
+               (add-rec (node-operator (token-val operator) mul0 mul1) remaining))]
             [else (values mul0 tokens)]))
 
     (call-with-values (lambda () (mul tokens)) add-rec))
@@ -408,11 +387,11 @@
             [(or (token-lt? (first tokens)) (token-le? (first tokens)))
              (let-values ([(operator) (first tokens)]
                           [(add1 remaining) (add (rest tokens))])
-               (relational-rec (node-operator add0 add1 (token-val operator)) remaining))]
+               (relational-rec (node-operator (token-val operator) add0 add1) remaining))]
             [(or (token-gt? (first tokens)) (token-ge? (first tokens)))
              (let-values ([(operator) (first tokens)]
                           [(add1 remaining) (add (rest tokens))])
-               (relational-rec (node-operator add1 add0 (reverse-compare (token-val operator))) remaining))]
+               (relational-rec (node-operator (reverse-compare (token-val operator)) add1 add0) remaining))]
             [else (values add0 tokens)]))
 
     (call-with-values (lambda () (add tokens)) relational-rec))
@@ -462,14 +441,14 @@
              (values (node-return expr0) (rest remaining))]
             [(token-if? (first tokens))
              (token-must-be token-lparen? (rest tokens) input)
-             (define-values (expr-pred remaining0) (expr (rest (rest tokens))))
+             (define-values (conditional remaining0) (expr (rest (rest tokens))))
              (token-must-be token-rparen? remaining0 input)
-             (define-values (stmt-true remaining1) (stmt (rest remaining0)))
+             (define-values (true-clause remaining1) (stmt (rest remaining0)))
              (cond [(token-else? (first remaining1))
-                    (define-values (stmt-false remaining2) (stmt (rest remaining1)))
-                    (values (node-if expr-pred stmt-true stmt-false)  remaining2)]
+                    (define-values (false-clause remaining2) (stmt (rest remaining1)))
+                    (values (node-if conditional true-clause false-clause)  remaining2)]
                    [else
-                    (values (node-if expr-pred stmt-true null) remaining1)])]
+                    (values (node-if conditional true-clause null) remaining1)])]
             [else
              (define-values (expr0 remaining) (expr tokens))
              (token-must-be token-stmt? remaining input)
@@ -594,10 +573,10 @@
 
 (define (generate-left-value node)
   (unless (node-local-variable? node)
-    (generate-error (format "left value must be local variable: ~a" (node-val node))))
+    (generate-error (format "left value must be local variable: ~a" (object-name node))))
 
   `("mov rax, rbp"
-    ,(format "sub rax, ~a" (node-offset node))
+    ,(format "sub rax, ~a" (node-local-variable-offset node))
     "push rax"))
 
 (define (generate nodes)
@@ -611,34 +590,34 @@
   (define (generate-rec node)
     (if (null? node)
         ""
-        (cond [(node-number? node) (push (node-val node))]
+        (cond [(node-number? node) (push (node-number-val node))]
               [(node-local-variable? node)
                `(,@(generate-left-value node)
                  ,@(generate-load-from-local-variable))]
               [(node-assign? node)
-               `(,@(generate-left-value (node-left node))
-                 ,@(generate-rec (node-right node))
+               `(,@(generate-left-value (node-assign-left node))
+                 ,@(generate-rec (node-assign-right node))
                  ,@(generate-store-to-local-variable))]
               [(node-return? node)
-               `(,@(generate-rec (node-left node))
+               `(,@(generate-rec (node-return-expr node))
                  ,@(pop-result)
                  ,@(free-local-variables)
                  ,@(return))]
               [(node-if? node)
                (define label-else (gen-label))
                (define label-end (gen-label))
-               `(,@(generate-rec (node-left node))
+               `(,@(generate-rec (node-if-conditional node))
                  ,@(list "pop rax"
                          "cmp rax, 0"
                          (format "je ~a" label-else))
-                 ,@(generate-rec (node-right node))
+                 ,@(generate-rec (node-if-true-clause node))
                  ,(format "jmp ~a" label-end)
                  ,(format "~a:" label-else)
-                 ,@(generate-rec (node-val node))
+                 ,@(generate-rec (node-if-false-clause node))
                  ,(format "~a:" label-end))]
               [(node-operator? node)
-               `(,@(generate-rec (node-left node))
-                 ,@(generate-rec (node-right node))
+               `(,@(generate-rec (node-operator-left node))
+                 ,@(generate-rec (node-operator-right node))
                  ,@(pop-operands)
                  ,@(cond [(node-add? node) (generate-add)]
                          [(node-sub? node) (generate-sub)]
@@ -649,7 +628,7 @@
                          [(node-lt? node) (generate-lt)]
                          [(node-le? node) (generate-le)]
                          [else
-                          (generate-error (format "unepexted operator: ~a" (node-val node)))])
+                          (generate-error (format "unexpected operator: ~a" (node-operator-op node)))])
                  ,@(push-result))])))
 
   (string-join
